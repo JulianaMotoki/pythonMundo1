b4b9d218e8dbb458af4178347381faa7c75c5367
Juliana <juliana.motoki@gmail.com>
2023-01-28 23:12:49 -0300

    Ajustes simples a partir da correção do exercicio, com a formatação das casas decimais por ser dinheiro e com a inclusão do R$ para evitar que o usuário inclua essa informação - 230128

--- exercicios/exe013.py
+++ exercicios/exe013.py
@@ -1,8 +1,10 @@
 # Desafio 013
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+# Acrescentando o R$ para evitar que o usuário insira essa informação
+# Também acrescentando a formatação de 2 casas decimais por ser um valor referente a dinheiro
 
-salario = float(input('Informe seu salário atual: '))
+salario = float(input('Informe seu salário atual: R$'))
 aumento = salario * 0.15
 salario_novo = salario + aumento
 
-print(f'Seu salário passa de R${salario} para R${salario_novo}, um aumento de R${aumento}')
+print(f'Seu salário passa de R${salario:.2f} para R${salario_novo:.2f}, um aumento de R${aumento:.2f}')
