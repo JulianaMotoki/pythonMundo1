e92fd23e83abfa395217b1c9c0339f0d9c278fbb
Juliana <juliana.motoki@gmail.com>
2023-01-28 22:15:35 -0300

    Upgrade do exercicio 8 com case para escolha da unidade de medida - 230128

--- exercicios/exe008_up.py
+++ exercicios/exe008_up.py
@@ -10,5 +10,20 @@ decimetro = metro * 10
 centimetro = metro * 100
 milimetro = metro * 1000
 
-print(f'{metro}m equivale a:\n{quilometro:.2f}km \n{hectometro:.2f}hm \n{decametro:.2f}dam \n{decimetro:.0f}dm \n{centimetro:.0f}cm \n{milimetro:.0f}mm')
+print('Unidades de Medida para Conversão \n1 - Quilômetro (km) \n2 - Hectômetro (hm) \n3 - Decâmetro (dam) \n4 - Decímetro (dm) \n5 - Centímetro (cm) \n6 - Milímetro (mm)')
 
+opcao = int(input('Escolha a unidade de medida a ser calculada: '))
+
+match (opcao):
+    case 1:
+        print(f'{metro}m equivale a {quilometro:.2f}km')
+    case 2:
+        print(f'{metro}m equivale a {hectometro:.2f}hm')
+    case 3:
+        print(f'{metro}m equivale a {decametro:.2f}dam')
+    case 4:
+        print(f'{metro}m equivale a {decimetro:.0f}dm')
+    case 5:
+        print(f'{metro}m equivale a {centimetro:.0f}cm')
+    case 6:
+        print(f'{metro}m equivale a {milimetro:.0f}mm')
