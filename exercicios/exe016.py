83fa2327ec27cdac6f5f190565c79f1498f27a36
Juliana <juliana.motoki@gmail.com>
2023-02-01 22:44:18 -0300

    Exercio 16 corrigito - 230201

--- exercicios/exe016.py
+++ exercicios/exe016.py
@@ -2,8 +2,8 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
 # Ex.: Digite um número: 6.127 O número 6.127 tem a parte inteira 6.
 
-from math import floor
+from math import trunc
 
-numero = floor(float(input('Informe um número: ')))
+numero = float(input('Informe um número: '))
 
-print(f'A porção inteira do número informado é: {numero2}')
+print(f'A porção inteira do 1º número informado é: {trunc(numero)}')
