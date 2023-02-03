eeaf67e6c26e359c6fb60dbe69f22f4ce906dd21
Juliana <juliana.motoki@gmail.com>
2023-02-02 23:27:23 -0300

    Exercicio 17 corrigido (Opção 2) - 230202

--- exercicios/exe017.py
+++ exercicios/exe017.py
@@ -1,10 +1,11 @@
 # Desafio 017
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 
-from math import sqrt
+from math import sqrt, hypot
 
 cateto_oposto = float(input('Informe o valor do cateto oposto: '))
 cateto_adjacente = float(input('Informe o valor do cateto adjacente: '))
-hipotenusa = sqrt((cateto_oposto ** 2) + (cateto_adjacente ** 2))
+# hipotenusa = sqrt((cateto_oposto ** 2) + (cateto_adjacente ** 2))
+hipotenusa = hypot(cateto_oposto, cateto_adjacente)
 
 print(f'O valor da hipotenusa é: {hipotenusa:.3f}')
